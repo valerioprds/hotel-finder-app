7f952a079201aba5e9252d429476f77e2db05e25
valerioprds <v.paradiso89@gmail.com>
2024-01-11 21:43:54 +0100

    main now occupies full screen

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,36 +1,46 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import styles from './Header.module.css'; // Import the styles
-import Image from 'next/image';
+import React, { useState } from "react";
+import styles from "./Header.module.css"; // Import the styles
+import Image from "next/image";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = () => setIsOpen(!isOpen);
+	const toggleDropdown = () => setIsOpen(!isOpen);
 
-    return (
+	return (
         <header className={styles.header}>
             <div className={styles.logo}>
                 {/* Replace with your actual logo */}
-                <Image 
-                    src="/Holiday Travel Suitcase Illustration Logo Design.png" // Adjust the path as needed
-                    alt="Logo" 
+                <Image
+                    src="/egruppa travel.png" // Adjust the path as needed
+                    alt="Logo"
                     className={styles.logoImage}
                     width={150}
                     height={150} // Set the height as needed
-                    layout="fill" // Can be 'fixed', 'responsive', 'intrinsic', or 'fill'
-                />            </div>
+                    layout="fixed" // Can be 'fixed', 'responsive', 'intrinsic', or 'fill'
+                />
+            </div>
             <div className={styles.menu}>
-                <button onClick={toggleDropdown} className={styles.dropdownButton}>
-                    Check Our Destinations 
+                <button
+                    onClick={toggleDropdown}
+                    className={styles.dropdownButton}
+                >
+                    Check Our Destinations
                 </button>
                 {isOpen && (
                     <div className={styles.dropdownContent}>
                         {/* Dropdown menu items */}
-                        <a href="#" className={styles.dropdownItem}>Catalonia</a>
-                        <a href="#" className={styles.dropdownItem}>Basque Country</a>
-                        <a href="#" className={styles.dropdownItem}>Canary Islands</a>
+                        <a href="#" className={styles.dropdownItem}>
+                            Catalonia
+                        </a>
+                        <a href="#" className={styles.dropdownItem}>
+                            Basque Country
+                        </a>
+                        <a href="#" className={styles.dropdownItem}>
+                            Canary Islands
+                        </a>
                     </div>
                 )}
             </div>
--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,7 @@ export default function Home() {
 	return (
 		<main>
 			<h2>Dashboard</h2>
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero
-				repellendus tempore, exercitationem odit, quasi doloremque
-				possimus recusandae alias sequi totam soluta natus iure eius,
-				obcaecati sint dolores blanditiis aspernatur quo officia iusto
-				ut. Et, aliquid sed voluptates iste cum totam, facere explicabo,
-				fugit suscipit ratione aspernatur consequuntur ex mollitia
-				quaerat?
-			</p>
+			<p> ... </p>
 
 			<div className="flex justify-center my-8">
 				<Link href="/tickets">
@@ -22,25 +14,35 @@ export default function Home() {
 
 			<h2>Company Updates</h2>
 
-			<div className="card">
-				<h3>New member of the web dev team...</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Incidunt, at quam. Dolores omnis possimus quam soluta rerum
-					illo laborum ullam pariatur molestiae, modi beatae corrupti.
-				</p>
-			</div>
-			<div className="card">
-				<h3>New website live!</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Incidunt, at quam. Dolores omnis possimus quam soluta rerum
-					illo laborum ullam pariatur molestiae, modi beatae corrupti,
-					assumenda distinctio adipisci, cupiditate minima eum vitae?
-					Similique dicta est facilis debitis, autem temporibus quo
-					repellat illum unde id iste veritatis eveniet, aspernatur
-					enim quas.
-				</p>
+			{/* Full-width Flex Container */}
+			<div className="flex flex-col md:flex-row mx-auto w-full">
+				<div className="w-11/12 md:w-1/2 px-2 mb-4 md:mb-0">
+					<div className="card">
+						<h3>New member of the web dev team...</h3>
+						<p> ... </p>
+					</div>
+				</div>
+
+				<div className="w-11/12 md:w-1/2 px-2">
+					<div className="card">
+						<h3>New website live!</h3>
+						<p> ... </p>
+					</div>
+				</div>
+
+				<div className="w-11/12 md:w-1/2 px-2">
+					<div className="card">
+						<h3>New website live!</h3>
+						<p> ... </p>
+					</div>
+				</div>
+
+				<div className="w-11/12 md:w-1/2 px-2">
+					<div className="card">
+						<h3>New website live!</h3>
+						<p> ... </p>
+					</div>
+				</div>
 			</div>
 		</main>
 	);
