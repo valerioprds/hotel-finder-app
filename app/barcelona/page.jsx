348da1646970c31644595f66608465531a5f7619
valerioprds <v.paradiso89@gmail.com>
2024-01-12 13:58:42 +0100

    button added

--- app/barcelona/page.jsx
+++ app/barcelona/page.jsx
@@ -1,13 +1,11 @@
 "use client";
+
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./BarcelonaComponent.module.css";
-//icons
 import LocationCityOutlinedIcon from "@mui/icons-material/LocationCityOutlined";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
-
-//buttons
 import Button from "@mui/material/Button";
 
 export default function HotelsList() {
@@ -47,11 +45,10 @@ export default function HotelsList() {
 			<div className="container mx-auto">
 				<h2 className="font-bold text-2xl mb-4">Hotels in Barcelona</h2>
 				<div className="flex flex-col">
-					{/* aqui estan las cards */}
 					{hotels.map((hotel, index) => (
 						<div
 							key={index}
-							className=" mb-4 bg-white rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row"
+							className="mb-4 bg-white rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row"
 						>
 							<div className="relative h-64 md:h-auto md:w-1/5">
 								<Image
@@ -61,9 +58,9 @@ export default function HotelsList() {
 									alt={hotel.name}
 								/>
 							</div>
-							<div className="flex flex-col justify-between p-4 leading-normal">
+							<div className="flex flex-col justify-between p-4 leading-normal flex-grow">
 								<div>
-									<h3 className="font-bold text-lg ">
+									<h3 className="font-bold text-lg">
 										{hotel.name}
 									</h3>
 									<p
@@ -80,15 +77,18 @@ export default function HotelsList() {
 										<LocationOnOutlinedIcon className="mr-1" />
 										{hotel.category}
 									</p>
-									{/* Display the rating */}
 									<p className="text-gray-700 text-sm">
 										<ThumbUpOutlinedIcon className="mr-1" />
 										Rating: {hotel.rating}
 									</p>
-									
 								</div>
 
-								{/* ... Additional hotel details ... */}
+								{/* Button Container */}
+								<div className="flex justify-end mt-4">
+									<Button variant="contained">
+										Add To Cart 
+									</Button>
+								</div>
 							</div>
 						</div>
 					))}
